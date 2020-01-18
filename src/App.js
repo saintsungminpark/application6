c727c765a0fdf1ee24b4a33fde44f8862a105304
saintsungminpark@gmail.com <saintsungminpark@gmail.com>
2020-01-18 18:56:23 +0900

    update: create customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system</h2>
-    </div>
-  );
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': 'Park',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': 'teacher'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': 'James',
+  'birthday': '201222',
+  'gender': '남자',
+  'job': 'college student'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': 'Cathey',
+  'birthday': '651222',
+  'gender': '여자',
+  'job': 'doctor'
+},
+]
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </Fragment>
+    );
+  }
 }
 
 export default App;
