df5f7c195099b6adee3d5dff464deb3fb56b4c71
saintsungminpark@gmail.com <saintsungminpark@gmail.com>
2020-01-21 04:25:15 +0900

    update: add get customers rest api module

--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': 'Park',
+            'birthday': '961222',
+            'gender': '남자',
+            'job': 'teacher'
+        },
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': 'James',
+            'birthday': '201222',
+            'gender': '남자',
+            'job': 'college student'
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': 'Cathey',
+            'birthday': '651222',
+            'gender': '여자',
+            'job': 'doctor'
+        },
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
